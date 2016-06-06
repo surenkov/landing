ab1344d5e437de3076e4c3e6b3c27f6e1553462c
Savva Surenkov <savva.surenkov@outlook.com>
2016-06-06 16:26:37 +1000

    Specify session storage as MongoDB.

--- landing/__init__.py
+++ landing/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_mongoengine import MongoEngine
+from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
 
 
 app = Flask(__name__)
 db = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(db)
 
 import landing.blocks
+import landing.models
 import landing.views
-from landing.manager import manager_app
 
+from landing.manager import manager_app
 app.register_blueprint(manager_app)
